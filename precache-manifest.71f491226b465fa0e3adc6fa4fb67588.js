dd0a4e1c02024ef57f2f24cb106d96e081b88735
Artak <artak.avanesyan93@gmail.com>
2018-12-16 16:08:14 +0200

    Updates

---
+++ precache-manifest.71f491226b465fa0e3adc6fa4fb67588.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "8d1fc6fd05ce1078046f",
+    "url": "/react-app/static/js/main.8d1fc6fd.chunk.js"
+  },
+  {
+    "revision": "859d87d4ccce3d768800",
+    "url": "/react-app/static/js/1.859d87d4.chunk.js"
+  },
+  {
+    "revision": "8d1fc6fd05ce1078046f",
+    "url": "/react-app/static/css/main.24e73591.chunk.css"
+  },
+  {
+    "revision": "dbb982fe909333b1ea8287da578df684",
+    "url": "/react-app/index.html"
+  }
+];
